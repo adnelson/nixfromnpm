6968bb1bfec0b6b56f2d5ca4d57c2e9be6f706aa
Allen Nelson <anelson@narrativescience.com>
2015-06-23 14:47:02 -0500

    using hnix instead of simple-nix

--- project.nix
+++ project.nix
@@ -1,27 +1,27 @@
 { mkDerivation, aeson, base, bytestring, classy-prelude
 , data-default, directory, error-list, filepath, github, hspec
 , hspec-expectations, http-client-streams, io-streams, MissingH
-, mtl, network-uri, parsec, shelly, simple-nix, stdenv
+, mtl, network-uri, parsec, shelly, hnix, stdenv
 , system-filepath, text, text-render, unordered-containers, pkgs
-, docopt
+, docopt, containers
 }:
 mkDerivation {
   pname = "nixfromnpm";
-  version = "0.1.0.6";
+  version = "0.1.0.7";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
     aeson base bytestring classy-prelude data-default directory
     error-list filepath MissingH mtl network-uri parsec shelly
-    simple-nix system-filepath text text-render unordered-containers
-    pkgs.curl docopt
+    hnix system-filepath text text-render unordered-containers
+    pkgs.curl docopt containers
   ];
   testDepends = [
     aeson base bytestring classy-prelude data-default directory
     error-list filepath github hspec hspec-expectations
     http-client-streams io-streams MissingH mtl network-uri parsec
-    shelly simple-nix system-filepath text text-render
+    shelly hnix system-filepath text text-render containers
     unordered-containers
   ];
   license = stdenv.lib.licenses.mit;
