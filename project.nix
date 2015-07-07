655b39ac3a1d309185e8d9d63f29c2fb2f02a049
Allen Nelson <anelson@narrativescience.com>
2015-07-07 10:48:45 -0500

    added issue tracker link;

--- project.nix
+++ project.nix
@@ -1,28 +1,26 @@
-{ mkDerivation, aeson, base, bytestring, classy-prelude
-, data-default, directory, error-list, filepath, github, hspec
-, hspec-expectations, http-client-streams, io-streams, MissingH
-, mtl, network-uri, parsec, shelly, hnix, stdenv
-, system-filepath, text, text-render, unordered-containers, pkgs
-, docopt, containers
+{ mkDerivation, aeson, base, bytestring, classy-prelude, containers
+, data-default, directory, docopt, error-list, filepath, github
+, hnix, hspec, hspec-expectations, http-client-streams, io-streams
+, MissingH, mtl, network-uri, parsec, shelly, stdenv
+, system-filepath, text, text-render, unordered-containers
 }:
 mkDerivation {
   pname = "nixfromnpm";
-  version = "0.1.0.7";
+  version = "0.1.0.8";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    aeson base bytestring classy-prelude data-default directory
-    error-list filepath MissingH mtl network-uri parsec shelly
-    hnix system-filepath text text-render unordered-containers
-    pkgs.curl docopt containers
+    aeson base bytestring classy-prelude containers data-default
+    directory docopt error-list filepath hnix MissingH mtl network-uri
+    parsec shelly system-filepath text text-render unordered-containers
   ];
   testDepends = [
-    aeson base bytestring classy-prelude data-default directory
-    error-list filepath github hspec hspec-expectations
+    aeson base bytestring classy-prelude containers data-default
+    directory error-list filepath github hnix hspec hspec-expectations
     http-client-streams io-streams MissingH mtl network-uri parsec
-    shelly hnix system-filepath text text-render containers
-    unordered-containers
+    shelly system-filepath text text-render unordered-containers
   ];
+  description = "Generate nix expressions from npm packages";
   license = stdenv.lib.licenses.mit;
 }
