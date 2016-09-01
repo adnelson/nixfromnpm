4759eee4bcc30204071146213218df598f46084d
Allen Nelson <anelson@narrativescience.com>
2016-08-31 20:03:33 -0500

    "nixfromnpm: bump version from 0.10.5 to 0.10.6"

--- project.nix
+++ project.nix
@@ -22,7 +22,7 @@ in
 mkDerivation {
   inherit src;
   pname = "nixfromnpm";
-  version = "0.10.5";
+  version = "0.10.6";
   isExecutable = true;
   buildDepends = [
     aeson base bytestring classy-prelude containers data-default
