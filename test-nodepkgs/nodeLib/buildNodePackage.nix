8e2906b551e55e8e7b155ff961c4ae1cf8843953
Allen Nelson <anelson@narrativescience.com>
2016-09-02 17:38:26 -0500

    separate doc into a markdown file

--- test-nodepkgs/nodeLib/buildNodePackage.nix
+++ test-nodepkgs/nodeLib/buildNodePackage.nix
@@ -54,83 +54,6 @@ let
     else if obj == null then {}
     else listToAttrs (map (x: nameValuePair x.name x)
                      (map verifyNodePackage obj));
-
-  # Npm supports circular dependencies, while nix does not. A package
-  # P has a circular dependency D if and only if P appears in the
-  # dependency closure of D. Imagine the following situation ("->"
-  # means "depends on"):
-  #
-  #  A -> {B}, B -> {A, C}, C -> {D, E, F}
-  #
-  # In this case we have circularity between A and B. When building A,
-  # we can't build B the normal way, because we'd have to first build
-  # A, and we'd get infinite recursion. However, we can put the
-  # package B, and all of its dependencies, in the `node_modules`
-  # folder for A, and then A will be able to use B and vice versa when
-  # A is a dependency of some library. This means to build A we need to:
-  #
-  # 1. Build C, D, E, and F the normal way.
-  # 2. Symlink C, D, E and F into A's node_modules folder.
-  # 3. Extract the source of B into the node_modules folder.
-  # 4. Make a self-referential symlink of A into its own node_modules folder.
-  #
-  # The last step is a bit strange but since B needs to be able to import
-  # A, A needs to be in the same node_modules folder as B. We can
-  # accomplish this with a symlink.
-  #
-  # Now, how about building B? A is a circular dependency of B, but A
-  # doesn't have any other dependencies. This means that the process
-  # is simply
-  #
-  # 1. Extract the source of A into the node_modules folder.
-  # 2. Symlink B into the node_modules folder.
-  #
-  # Now let's imagine a slightly more complicated situation involving
-  # a three-way circularity.
-  #
-  # A -> {B, C}, B -> {C, D, E}, C -> {A, D}
-  #
-  # In this case the only packages we can build normally are D and
-  # E. Then to build A, we build E and D as normal and symlink them
-  # into node_modules, extract B and C's source into node_modules, and
-  # make a reflexive symink.
-  #
-  # Finally, a double-circular dependency:
-  #
-  # A -> {B}, B -> {C, A}, C -> {B}
-  #
-  # So the more general algorithm to build a package A which has one
-  # or more circular dependencies is:
-  #
-  # 1. Compute the full set of packages that those dependencies depend
-  #    on at runtime (including transitive dependencies), *except* A and
-  #    the packages themselves.
-  # 2. Symlink all of these into node_modules.
-  # 3. Extract the source of each circular dependency into node_modules.
-  # 4. Make a self-referential symlink of A.
-
-  # So the first step is to compute the closure of a package's
-  # circular dependencies.  We need to keep track of the names of
-  # packages we've already encountered to avoid loops.
-  cycles =
-    # Packages we've already seen.
-    seenPackages:
-    # Name of package we're inspecting.
-    package:
-      if hasAttr package.name seenPackages
-      # We've completed the cycle; stop here.
-      then seenPackages
-      else let
-        # Add package to seen.
-        seen = seenPackages // {"${package.name}" = package;};
-        # Recur on circular dependencies.
-        cycles' = map (cycles seen) (attrValues package.circularDependencies);
-      in
-      # Combine the results into a single set.
-      foldl (a: b: a // b) {} cycles';
-
-  # We'll put all of these together below, for packages that have
-  # circular dependencies.
 in
 
 {
@@ -368,8 +291,28 @@ let
       runHook postPatch
     '';
 
+    # Computes the "circular closure" of a package.
+    # See ./circular_dependencies.md for details.
+    circularClosure =
+      # Packages we've already seen.
+      seenPackages:
+      # Name of package we're inspecting.
+      package:
+        if hasAttr package.name seenPackages
+        # We've completed the cycle; stop here.
+        then seenPackages
+        else let
+          # Add package to seen.
+          seen = seenPackages // {"${package.name}" = package;};
+          # Recur on circular dependencies.
+          closure = map (circularClosure seen)
+                        (attrValues package.circularDependencies);
+        in
+        # Combine the results into a single set.
+        foldl (a: b: a // b) {} closure;
+
     # Compute any cycles. Remove 'self' from the dependency closure.
-    circularDepClosure = removeAttrs (cycles {} self) [self.name];
+    circularDepClosure = removeAttrs (circularClosure {} self) [self.name];
 
     # Turn the closure into a list of all circular dependencies.
     circulars = attrValues circularDepClosure;
