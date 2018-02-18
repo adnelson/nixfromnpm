c5b4744face21439588e89d1d3cfb0eaad5c8122
Allen Nelson <adnelson@users.noreply.github.com>
2018-02-17 18:21:34 -0600

    add CircleCI config with tests (#101)
    * add travis.yml
    
    * Make sure we include `namespaces` in the top-level scope (#99)
    
    * start working on test script
    
    * add integration test script
    
    * use Popen/PIPE
    
    * try adding circleci
    
    * change to retrigger circleci
    
    * see where the circle ci pwd is
    
    * run tests
    
    * make tests executable
    
    * use sh instead of bash
    
    * use PWD to try to be "safer" in filtering the src directory
    
    * be more verbose when running with CI
    
    * output is not showing for some reason
    
    * manually write out circle tests
    
    * add more tests
    
    * use 2 processes instead of nproc

--- release.nix
+++ release.nix
@@ -8,34 +8,38 @@ let
     (newPkgs: oldPkgs: {
 
       haskellPackages = oldPkgs.haskellPackages.override {
-        overrides = haskellPackagesNew: haskellPackagesOld: 
-          let
-            inherit (newPkgs.lib) any flip elem hasSuffix hasPrefix;
-            # We'll typically have a lot of files in this directory; we only want
-            # to take a few of them though.
-            filesToExclude = ["node_modules" "shell.nix" "project.nix" "dist" "scripts" "README.md" ];
-            suffixesToExclude = ["-test"];
-            filter = baseName: (!elem baseName filesToExclude) &&
-                               (!hasPrefix "." baseName) &&
-                               (!any (flip hasSuffix baseName) suffixesToExclude);
-
-            src = builtins.filterSource (path: _: filter (baseNameOf path)) ./.;
-          in
-            { semver-range = 
+        overrides = haskellPackagesNew: haskellPackagesOld:
+            { semver-range =
                 haskellPackagesNew.callPackage ./nix/semver-range.nix { };
 
               text-render =
                 haskellPackagesNew.callPackage ./nix/text-render.nix { };
 
               nixfromnpm =
+                let
+                  inherit (newPkgs.lib) any flip elem hasSuffix hasPrefix elemAt splitString;
+                  # We'll typically have a lot of files in this directory;
+                  # we only want to take a few of them though. Make a filtering
+                  # function which will choose them.
+                  dirsToInclude = ["src" "tests" "nix-libs"];
+                  filesToInclude = ["LICENSE" "nixfromnpm.cabal"];
+                  _filter = path: type: let
+                    # NOTE: using PWD here is hacky; means its
+                    # behavior depends on the directory the command is
+                    # being executed from. Fix me!
+                    subpath = elemAt (splitString "${builtins.getEnv "PWD"}/" path) 1;
+                    spdir = elemAt (splitString "/" subpath) 0;
+                  in elem spdir dirsToInclude ||
+                     (type == "regular" && elem subpath filesToInclude);
+                in
                 newPkgs.haskell.lib.overrideCabal
                   (haskellPackagesNew.callPackage ./default.nix { })
                   (oldDerivation: rec {
-                    src = builtins.filterSource (path: _: filter (baseNameOf path)) oldDerivation.src;
-                    shellHook = (oldDerivation.shellHook or "") + ''
+                    src = builtins.filterSource _filter oldDerivation.src;
+                    shellHook = builtins.trace src ((oldDerivation.shellHook or "") + ''
                       export SRC=${src}
                       export CURL_CA_BUNDLE=${newPkgs.cacert}/etc/ssl/certs/ca-bundle.crt
-                    '';
+                    '');
                   });
             };
       };
@@ -47,4 +51,4 @@ let
 
 in
 
-  { inherit (pkgs.haskellPackages) nixfromnpm; }
+  { inherit (pkgs.haskellPackages) nixfromnpm; inherit pkgs; }
