e9c4173acb547c42fdd704cf805c9ad3fd0aa0f0
Allen Nelson <ithinkican@gmail.com>
2018-06-25 14:22:39 -0500

    add a comment

--- src/NixFromNpm/Npm/Version.hs
+++ src/NixFromNpm/Npm/Version.hs
@@ -86,6 +86,15 @@ instance FromJSON NpmVersionRange where
     String s -> case parseNpmVersionRange s of
       Nothing -> return $ InvalidVersion s
       Just range -> return range
+    -- NOTE: I couldn't find a mention of this format for specifying
+    -- versions in the official package.json documentation. However,
+    -- this was encountered in the wild:
+    --
+    --   https://github.com/adnelson/nixfromnpm/issues/138
+    --
+    -- Making a special case for it here seems pretty harmless,
+    -- especially if it was at one point supported. Alternatively, we
+    -- could allow parsing dev dependencies and such-like to fail.
     Object m -> case H.lookup "version" m of
       Just v -> parseJSON v
       Nothing -> fail "no 'version' key found in dependency object"
